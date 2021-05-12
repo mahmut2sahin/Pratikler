8a30b38a98ca91082e11e928e682e1ff572e13b4
Mahmut Sahin <mahmut2sahin@gmail.com>
2021-05-13 01:02:59 +0300

    pratik-1-tamam

---
+++ console.programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console.programlama
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      Console.WriteLine("What is your name?");
+      string name = Console.ReadLine();
+      Console.WriteLine("What is your surname?");
+      string surname = Console.ReadLine();
+      Console.WriteLine("Hello " + name + " " + surname);
+    }
+  }
+}
