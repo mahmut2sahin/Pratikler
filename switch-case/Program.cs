6953e1f0179b85c6144e4dff4dc2c32da2866c46
mahmut sahin <mahmut2sahin@gmail.com>
2021-05-14 16:58:22 +0300

    switch-case-tamam

---
+++ switch-case/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace switch_case
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int month = DateTime.Now.Month;
+
+      switch (month)
+      {
+        case 1:
+          Console.WriteLine("Ocak ayindasiniz");
+          break;
+        case 2:
+          Console.WriteLine("Subat ayindasiniz");
+          break;
+        case 4:
+          Console.WriteLine("Nisan ayindasiniz");
+          break;
+        case 3:
+          Console.WriteLine("Mart ayindasiniz");
+          break;
+        default:
+          Console.WriteLine("Yanlis veri girdiniz!");
+          break;
+      }
+
+      switch (month)
+      {
+        case 12:
+        case 1:
+        case 2:
+          Console.WriteLine("Kis mevsimindesiniz");
+          break;
+        case 3:
+        case 4:
+        case 5:
+          Console.WriteLine("İlkbahar mevsimindesiniz");
+          break;
+        case 6:
+        case 7:
+        case 8:
+          Console.WriteLine("Yaz mevsimindesiniz");
+          break;
+        default:
+          break;
+      }
+    }
+  }
+}
