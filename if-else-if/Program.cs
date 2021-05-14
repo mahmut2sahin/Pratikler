d914e5ec102db329a4d0fc24364d00180497c01a
mahmut sahin <mahmut2sahin@gmail.com>
2021-05-14 16:46:56 +0300

    if-else-if-tamam

---
+++ if-else-if/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace if_else_if
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int time = DateTime.Now.Hour;
+
+      if (time >= 6 && time < 11)
+        Console.WriteLine("Gunaydin!");
+      else if (time <= 18)
+        Console.WriteLine("Iyı gunler!");
+      else
+        Console.WriteLine("Iyı geceler!");
+
+      string sonuc = time <= 18 ? "Iyı gunler!" : "Iyı geceler!";
+      sonuc = time >= 6 && time <= 11 ? "Gunaydin" : time <= 18 ? "Iyı gunler" : "Iyı geceler";
+    }
+  }
+}
