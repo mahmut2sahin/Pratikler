81faf2a59e06f9d8c704e4565ee4e6b35b66468a
mahmut sahin <mahmut2sahin@gmail.com>
2021-05-14 22:35:16 +0300

    hazir-metotlar-string-tamam

---
+++ hazir-metotlar-string/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace hazir_metotlar_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string degisken = "Dersimiz CSharp, Hoşgeldiniz!";
+            string degisken2 = "Dersimiz CSharp";
+            //Length
+            Console.WriteLine(degisken.Length);
+
+            //ToUpper, ToLower
+            Console.WriteLine(degisken.ToUpper());
+            Console.WriteLine(degisken.ToLower());
+
+            //Concat
+            Console.WriteLine(String.Concat(degisken, " Merhaba!"));
+
+            //Compare, CompareTo
+            Console.WriteLine(degisken.CompareTo(degisken2)); //return -1, 0, 1
+            Console.WriteLine(String.Compare(degisken, degisken2, true)); //return -1, 0, 1
+            Console.WriteLine(String.Compare(degisken, degisken2, false)); //return -1, 0, 1
+
+            //Contains
+            Console.WriteLine(degisken.Contains(degisken2));
+            Console.WriteLine(degisken.EndsWith("Hoşgeldiniz!"));
+            Console.WriteLine(degisken.StartsWith("Merhaba!"));
+
+            //IndexOf
+            Console.WriteLine(degisken.IndexOf("CS")); //returns index of first character
+            Console.WriteLine(degisken.IndexOf("Martin")); // returns -1
+
+            //PadLeft, PadRight
+            Console.WriteLine(degisken + degisken2.PadLeft(30));
+            Console.WriteLine(degisken.PadRight(50, '-') + degisken2);
+            Console.WriteLine(degisken + degisken2.PadLeft(30, '*'));
+            Console.WriteLine(degisken.PadRight(50) + degisken2);
+
+            //Replace
+            Console.WriteLine(degisken.Replace("CSharp", "C#"));
+
+            //Split
+            Console.WriteLine(degisken.Split(' ')[1]);
+
+            //Substring
+            Console.WriteLine(degisken.Substring(4, 6));
+        }
+    }
+}
