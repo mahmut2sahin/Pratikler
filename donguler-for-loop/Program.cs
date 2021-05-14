325cebd3f6601653d657071602e6351acbb98685
mahmut sahin <mahmut2sahin@gmail.com>
2021-05-14 17:20:34 +0300

    donguler-for-loop

---
+++ donguler-for-loop/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace donguler_for_loop
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int sayac = int.Parse(Console.ReadLine());
+      for (int i = 0; i <= sayac; i++)
+      {
+        if (i%2 == 1)
+        {
+          Console.WriteLine(i);
+        }
+      }
+      
+      int tektoplam = 0;
+      int cifttoplam = 0;
+      for (int j = 1; j < 1000; j++)
+      {
+        if (j%2 == 1)
+        {
+          tektoplam += 1;
+        }
+        else
+        {
+          cifttoplam += 1;
+        }
+      }
+      
+      Console.WriteLine("Tek Toplam: " + tektoplam);
+      Console.WriteLine("Cift Toplam: " + cifttoplam);
+
+      for (int k = 0; k < 10; k++)
+      {
+        if (k == 4)
+        {
+          break;
+        }
+        Console.WriteLine(k);
+      }
+
+      for (int l = 0; l < 10; l++)
+      {
+        if (l == 4)
+        {
+          continue;
+        }
+        Console.WriteLine(l);
+      }
+    }
+  }
+}
