6c5a9b8f26b428de1faef626dfc21d0e020c3023
mahmut sahin <mahmut2sahin@gmail.com>
2021-05-14 23:22:44 +0300

    arraylist-tamam

---
+++ arraylist/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace arraylist
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ArrayList liste = new ArrayList();
+      liste.Add("Mahmut");
+      liste.Add(24);
+      liste.Add(true);
+      liste.Add('M');
+
+      Console.WriteLine(liste[1]);
+
+      foreach (var item in liste)
+      {
+        Console.WriteLine(item);
+      }
+
+      string[] renkler = { "kırmızı", "sarı", "yeşil" };
+      List<int> sayılar = new List<int>(){1, 8, 23, 9, 92, 3};
+      liste.AddRange(renkler);
+      liste.AddRange(sayılar);
+
+      foreach (var item in liste)
+      {
+        Console.WriteLine(item);
+      }
+
+      liste.Sort();
+
+      liste.BinarySearch(9);
+
+      liste.Reverse();
+      foreach (var item in liste)
+      {
+        Console.WriteLine(item);
+      }
+
+      liste.Clear();
+    }
+  }
+}
+
