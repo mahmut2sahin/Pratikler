4c651232c5ab1304f0f7ab3bb92db6a18d132c6a
mahmut sahin <mahmut2sahin@gmail.com>
2021-05-16 04:46:14 +0300

    static-sinif-ve-uyeler-tamam

---
+++ static-sinif-ve-uyeler/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace static_sinif_ve_uyeler
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Çalışan sayısı: " + Calisan.CalisanSayisi);
+
+      Calisan calisan0 = new Calisan("Mahmut", "Sahin", "CE");
+      Console.WriteLine("Çalışan sayısı: " + Calisan.CalisanSayisi);
+      Calisan calisan1 = new Calisan("Marin", "Eden", "CAT");
+      Calisan calisan2 = new Calisan("Çaçaleyte", "Ims", "POMP");
+
+      Console.WriteLine("Çalışan sayısı: " + Calisan.CalisanSayisi);
+
+      Console.WriteLine(Islemler.Cikar(100, 200));
+      Console.WriteLine(Islemler.Topla(100, 200));
+    }
+  }
+
+  class Calisan
+  {
+    private static int calisanSayisi;
+    public static int CalisanSayisi { get => calisanSayisi;}
+    private string Isim;
+    private string Soyisim;
+    private string Departman;
+    static Calisan()
+    {
+      calisanSayisi = 0;
+    }
+
+    public Calisan(string isim, string soyisim, string departman)
+    {
+      Isim = isim;
+      Soyisim = soyisim;
+      Departman = departman;
+      calisanSayisi++;
+    }
+  }
+
+  static class Islemler
+  {
+    public static long Topla (int sayi1, int sayi2)
+    {
+      return sayi1 + sayi2;
+    }
+    public static long Cikar (int sayi1, int sayi2)
+    {
+      return sayi1 - sayi2;
+    }
+  }
+}
